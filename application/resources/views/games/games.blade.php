e864fc168711911b3a4b414a3742a74401906e16
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-05-23 21:07:24 +0800

    to master

--- application/resources/views/games/games.blade.php
+++ application/resources/views/games/games.blade.php
@@ -26,18 +26,22 @@
                     <table class="table table-dark table-hover">
                         <thead>
                           <tr>
-                            <th>Firstname</th>
-                            <th>Lastname</th>
+                            <th>Game</th>
+                            <th>Date</th>
                             <th>Email</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
                           </tr>
                         </thead>
                         <tbody>
                             @foreach ($games as $game)
                           <tr>
 
-                            <td>John</td>
+                            <td></td>
                             <td>Doe</td>
                             <td>john@example.com</td>
+                            <td><a href="{{route('game.edit',['id'=>$game])}}" class=" btn btn-primary">Edit</a></td>
+                            <td></td>
                           </tr>
                           @endforeach
                         </tbody>
@@ -51,42 +55,33 @@
 
 
     <!-- Side Content -->
-    <div class="relative text-2xl text-white sm:text-center xl:text-center lg:text-center bg-zinc-800 h-dvh w-80 col-1">
+    <div class="relative overflow-hidden text-2xl text-white sm:text-center xl:text-center lg:text-center bg-zinc-800 h-dvh w-80 col-1">
         <div class="relative rounded-full bg-zinc-500 md:h-80 md:w-200px top-2">
-
             <img src="" class="relative object-cover w-full h-full rounded-full" alt="Logo">
-
-
-
-
-
-
-
-
         </div>
         <p class="relative top-2">University of Perpetual Help System Dalta - Molino Campus</p>
         <div class="relative">
-            <div class="relative flex items-center rounded-tl-full rounded-tr-full rounded-bl-full rounded-br-full cursor-pointer justify-self-auto top-6 hover:bg-sky-700 w-50">
+            <div class="relative flex items-center w-auto rounded-tl-full rounded-tr-full rounded-bl-full rounded-br-full cursor-pointer top-6 hover:bg-sky-700">
                 <i class="relative fas fa-tachometer-alt left-20"></i>
                 <a href="{{route('game.index')}}" class="relative left-24">Dashboard</a>
             </div>
-            <div class="relative flex items-center h-auto gap-1 mt-4 rounded-tl-full rounded-tr-full rounded-bl-full rounded-br-full cursor-pointer justify-self-auto top-6 row col-1 hover:bg-sky-700 w-50">
+            <div class="relative flex items-center w-auto gap-1 mt-4 rounded-full cursor-pointer top-6 hover:bg-sky-700">
                 <i class="relative fas fa-basketball-ball left-20"></i>
                 <a href="" class="relative left-24">Games</a>
             </div>
-            <div class="relative flex items-center h-auto gap-1 mt-4 rounded-tl-full rounded-tr-full rounded-bl-full rounded-br-full cursor-pointer justify-self-auto top-6 row col-1 hover:bg-sky-700 w-50">
+            <div class="relative flex items-center w-auto gap-1 mt-4 rounded-full cursor-pointer top-6 hover:bg-sky-700">
                 <i class="relative fas fa-users left-20"></i>
                 <a href="#" class="relative left-24">Users</a>
             </div>
-            <div class="relative flex items-center h-auto gap-1 mt-4 rounded-tl-full rounded-tr-full rounded-bl-full rounded-br-full cursor-pointer justify-self-auto top-6 row col-1 hover:bg-sky-700 w-50">
+            <div class="relative flex items-center w-auto gap-1 mt-4 rounded-full cursor-pointer top-6 hover:bg-sky-700">
                 <i class="relative fas fa-trophy left-20"></i>
                 <a href="{{route('game.create')}}" class="relative left-24">Team Rankings</a>
             </div>
-            <div class="relative flex items-center h-auto gap-1 mt-4 rounded-tl-full rounded-tr-full rounded-bl-full rounded-br-full cursor-pointer justify-self-auto top-6 row col-1 hover:bg-sky-700 w-50">
+            <div class="relative flex items-center w-auto gap-1 mt-4 rounded-full cursor-pointer top-6 hover:bg-sky-700">
                 <i class="relative fas fa-cog left-20"></i>
                 <a href="#" class="relative left-24">Settings</a>
             </div>
-            <div class="relative flex items-center h-auto gap-1 mt-4 rounded-tl-full rounded-tr-full rounded-bl-full rounded-br-full cursor-pointer justify-self-auto top-6 row col-1 hover:bg-sky-700 w-50">
+            <div class="relative flex items-center w-auto gap-1 mt-4 rounded-full cursor-pointer top-6 hover:bg-sky-700">
                 <i class="relative fas fa-sign-out left-20"></i>
                 <a href="#" class="relative left-24">Sign out</a>
             </div>
