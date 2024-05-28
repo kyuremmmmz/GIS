1bae8b62621ba86d59f0337d2a0de74eafbfb08d
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-05-28 11:44:40 +0800

    to main haha

--- application/routes/web.php
+++ application/routes/web.php
@@ -4,8 +4,19 @@ use App\Http\Controllers\AdminLoginController;
 use App\Http\Controllers\GameAdminController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/games/games', function () {
+    return view('games.games');
+})->middleware(['auth', 'verified'])->name('game1.index');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/games/index', [GameAdminController::class,  'index'])->name('game.index');
+    Route::get('/games/create', [GameAdminController::class, 'create'])->name('game.create');
+    Route::post('/games/store', [GameAdminController::class, 'store'])->name('game.store');
+    Route::get('/games/{id}/edit', [GameAdminController::class, 'edit'])->name('game.edit');
     Route::get('/games/games', [GameAdminController::class, 'games'])->name('game1.index');
     Route::put('/games/{id}/update', [GameAdminController::class, 'update'])->name('game.update');
     Route::delete('/games/{id}/delete', [GameAdminController::class, 'delete'])->name('game.delete');
@@ -19,5 +30,19 @@ Route::middleware('auth')->group(function () {
 
 });
 
+// This Route function is for email verification sendings
+Route::get('/admin/verification', function()
+{
+    return view('admin.verification');
+})->middleware('auth')->name('verification.sent');
 
 
+// TODO: Make a function that i can request sendings to my gmail account
+
+Route::get('admin/verification/{id}/{hash}', function(EmailVerificationRequest $request){
+    $request->fulfill();
+
+    return redirect(route('verification.sent'));
+})->middleware(['auth', 'signed'])->name('email.requestSend');
+
+Route::post('/admin/');
