da5c621adeaf1978bb97e7d859afa97821297d12
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-06-05 22:46:19 +0800

    to master

--- application/app/Http/Controllers/ComitteeTeamController.php
+++ application/app/Http/Controllers/ComitteeTeamController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\comittee;
 
 use App\Http\Controllers\Controller;
+use App\Models\teams;
 use Illuminate\Http\Request;
 
 class ComitteeTeamController extends Controller
 {
-    //
+    public function comitteeTeamsView()
+    {
+        $data = teams::select('*')->orderBy('team', 'asc')->get();
+        return view('comittee/teams', compact('data'));
+    }
 }
