793f9076a3e498ec580ed47a6678d6ebecaa9df5
Yusako90 <141408182+Yusako90@users.noreply.github.com>
2024-06-09 15:36:52 +0800

    to master

--- application/app/Models/User.php
+++ application/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use App\Notifications\ComitteeResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -17,13 +16,11 @@ class User extends Authenticatable implements MustVerifyEmail
      *
      * @var array<int, string>
      */
+    protected $table = 'users';
     protected $fillable = [
         'Adminname',
-        'name',
         'email',
-        'comitteeEmail',
         'adminID',
-        'comitteeID',
         'password',
         'role'
     ];
@@ -51,15 +48,5 @@ class User extends Authenticatable implements MustVerifyEmail
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-    /**
-     * Send the password reset notification.
-     *
-     * @param  string  $token
-     * @return void
-     */
-    public function sendPasswordResetNotification($token)
-    {
-        $this->notify(new ComitteeResetPassword($token));
     }
 }
