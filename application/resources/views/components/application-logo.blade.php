cb06af628e3f950254b3c29ce68b45995e720273
zynyl <sunzynyl05@gmail.com>
2024-06-10 18:07:51 +0800

    to mastyer

--- application/resources/views/components/application-logo.blade.php
+++ application/resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center justify-center bg-black w-96">
-    <img class="h-24 rounded-full size-10" src="https://scontent.fcrk4-2.fna.fbcdn.net/v/t1.15752-9/361839816_594450056207554_5067445039035230165_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH7aVd9xyUPlxsDrw1HGupokKj6k4-Hb5-QqPqTj4dvn469EAukG5ZNsWA_cyMs37BNKmn5QYbzvKCBQ6xFxPf4&_nc_ohc=84g4L3woZVQQ7kNvgFXN0RZ&_nc_ht=scontent.fcrk4-2.fna&oh=03_Q7cD1QFSljQWGhWTfu5mpa4p0iI8TNz8y-Th9RLoJQYlV7_W8w&oe=667D3269" alt="">
-    <h1 class="text-white  text-wrap text-xl-center">University of Perpetual Help System Dalta - Molino Campus Game Information System</h1><br>
+<div class="flex items-center justify-center  w-96">
+    <img class=" h-96 rounded-full size-10" src="https://scontent.fcrk4-2.fna.fbcdn.net/v/t1.15752-9/361839816_594450056207554_5067445039035230165_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeH7aVd9xyUPlxsDrw1HGupokKj6k4-Hb5-QqPqTj4dvn469EAukG5ZNsWA_cyMs37BNKmn5QYbzvKCBQ6xFxPf4&_nc_ohc=84g4L3woZVQQ7kNvgFXN0RZ&_nc_ht=scontent.fcrk4-2.fna&oh=03_Q7cD1QFSljQWGhWTfu5mpa4p0iI8TNz8y-Th9RLoJQYlV7_W8w&oe=667D3269" alt="">
+    <h1 class="text-black  text-wrap text-xl-center">University of Perpetual Help System Dalta - Molino Campus Game Information System</h1><br>
 </div>
